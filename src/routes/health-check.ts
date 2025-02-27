c7fa2e129e272b016f6c4366b10d9356d471adbb
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2025-02-27 10:10:31 -0300

    fix: remove unused import

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -1,7 +1,4 @@
 import type { FastifyInstance } from "fastify";
-import { UploadImageToStorage } from "../functions/upload-image-to-storage";
-import { R2StorageProvider } from "../storage/providers/r2-storage";
-
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
