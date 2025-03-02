d1a3e124f01dfd6e21f67e29d84da6fc552732f3
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2025-03-02 05:15:01 -0300

    test

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
-    await reply.status(200).send({ messe: 'Ok ok!!!' })
+    await reply.status(200).send({ message: 'Ok ok!!!' })
   })
 }
